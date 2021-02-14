df26147800c2397d7d827b721bbaf94937d67706
shehanblast <shihanbinuka@gmail.com>
2021-02-14 20:30:05 +0530

    Create Country Details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryDetailsComponent } from './country-details/country-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryDetailsComponent
   ],
   imports: [
     BrowserModule,
