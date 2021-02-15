c36d14bd6ae0836013dc0a7ccf2ffe43d324997b
shehanblast <shihanbinuka@gmail.com>
2021-02-15 13:12:57 +0530

    Apply CSS changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CountryDetailsComponent} from './country-details/country-details.component';
 import {AddCountryComponent} from './add-country/add-country.component';
+import {HeaderComponent} from './header/header.component';
 
 
 const routes: Routes = [
   { path: '', component: CountryDetailsComponent  },//default path
   { path: 'add/:id', component: AddCountryComponent  },
-  {path: 'conn', component: CountryDetailsComponent}
+  {path: 'conn', component: CountryDetailsComponent},
+  {path: 'head', component: HeaderComponent}
 ];
 
 @NgModule({
